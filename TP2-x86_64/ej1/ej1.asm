7c829e3216576fff5093efce7a7ecca7880fb166
fedegutman <fgutman@udesa.edu.ar>
2025-04-18 13:30:11 -0300

    cambios

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -80,7 +80,6 @@ string_proc_list_add_node_asm:
     movzx r12, sil
     mov rsi, rdx
     movzx edi, r12b
-
     call string_proc_node_create_asm
     cmp rax, NULL
     je .return_null
@@ -89,7 +88,7 @@ string_proc_list_add_node_asm:
     jne .not_empty
 
     mov [rbx], rax
-    mov [rbx + 8], rax
+    mov [rbx + 8], rax ; first y last apuntan al nodo
     jmp .end
 
 .not_empty:
@@ -110,96 +109,3 @@ string_proc_list_add_node_asm:
     jmp .end
 
 string_proc_list_concat_asm:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16
-    push rbx
-    push r12
-    push r13
-    push r14
-    push r15
-
-    mov rbx, rdi        ; list
-    movzx r12, sil      ; type
-    mov r13, rdx        ; hash
-
-    ; Chequear si list o hash son NULL
-    test rbx, rbx
-    jz .return_null
-    test r13, r13
-    jz .return_null
-
-    ; Calcular la longitud de la cadena hash (strlen) y asignar memoria
-    xor rax, rax        ; rax = contador
-.len_loop:
-    cmp byte [r13 + rax], 0
-    je .len_done
-    inc rax
-    jmp .len_loop
-.len_done:
-    inc rax             ; Incluir terminador nulo
-    mov rdi, rax
-    call malloc
-    test rax, rax
-    jz .error
-
-    ; Copiar hash al buffer resultante
-    mov r14, rax        ; r14 = result
-    mov rsi, r13        ; source = hash
-    mov rdi, r14        ; destination = result
-.copy_loop:
-    mov cl, [rsi]
-    mov [rdi], cl
-    test cl, cl
-    jz .copy_done
-    inc rsi
-    inc rdi
-    jmp .copy_loop
-.copy_done:
-    mov r15, [rbx]      ; current_node = list->first
-
-.loop:
-    test r15, r15
-    jz .success
-
-    movzx eax, byte [r15 + 16]  ; current_node->type
-    cmp al, r12b
-    jne .next
-
-    mov rdi, r14
-    mov rsi, [r15 + 24] ; current_node->hash
-    call str_concat
-    test rax, rax
-    jz .error
-
-    mov rdi, r14
-    mov r14, rax
-    call free
-
-.next:
-    mov r15, [r15]      ; current_node = current_node->next
-    jmp .loop
-
-.success:
-    mov rax, r14
-    jmp .cleanup
-
-.error:
-    mov rdi, r14
-    call free
-    xor rax, rax
-    jmp .cleanup
-
-.return_null:
-    xor rax, rax
-    jmp .cleanup
-
-.cleanup:
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop rbx
-    add rsp, 16
-    pop rbp
-    ret
